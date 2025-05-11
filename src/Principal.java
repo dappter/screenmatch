a454eaa8d97c6bb095ae7a959b4617ab2ac7fdb4
José Guilherme R. <guilhermelossio8@gmail.com>
2025-05-11 16:32:06 -0300

    adicionando Filmes e Series

---
+++ src/Principal.java
@@ -0,0 +1,60 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.setNome("O poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        lost.exibeFichaTecnica();
+
+
+
+        System.out.println("Duração para maratonar lost: " + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora  = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+
+        System.out.println("Tempo total: " + calculadora.getTempoTotal());
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtrar(meuFilme);
+        filtro.filtrar(outroFilme);
+        filtro.filtrar(episodio);
+    }
+}
