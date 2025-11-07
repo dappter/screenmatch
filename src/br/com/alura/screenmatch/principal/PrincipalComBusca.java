7dcaf12573fe7566058a87431f4b1f431fd9e33c
José Guilherme R. <guilhermelossio8@gmail.com>
2025-11-07 17:17:39 -0300

    adicionar sistema de busca para filmes utilizando API

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o filme que quer buscar: ");
+        var busca = scanner.nextLine();
+        var endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=b32ab834";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=b32ab834"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
